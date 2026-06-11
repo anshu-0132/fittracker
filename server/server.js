fe9c51067005f5b79eee77033aac45feb4a0b8f0
Divyanshu Gupta <divyanshu0132@gmail.com>
2026-06-11 10:51:36 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+// import cors from "cors";
+import pg from "pg"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    port:process.env.DB_PORT,
+    database:process.env.DB_DATABASE,
+    host:process.env.DB_HOST
+})
+
+db.connect();
+
+// app.use(cors());
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    res.send("fittrack api running")
+})
+
+app.post("/submit",(req,res)=>{
+
+})
+
+app.listen(port,(req,res)=>{
+    console.log(`Server running on port ${port}`);
+})
